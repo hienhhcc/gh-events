b4fde8e4bf0da518d37d488dc6a8aba94c0eabd4
vuvinhhien <hien.vuvinh@hdwebsoft.dev>
2023-03-06 16:01:42 +0700

    Adjust h2->h5

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h5>{title}</h5>
       <p>{text}</p>
     </article>
   );
